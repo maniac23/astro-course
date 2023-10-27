e1fd072bb308d0518d83271c697236e7140e8f68
Mikhail <m.tukai@reply.com>
2023-10-27 15:14:53 +0300

    added rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,25 @@
+import rss from '@astrojs/rss';
+import type { AstroConfig } from 'astro';
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
+
+export async function get(context: AstroConfig) {
+	const blog = await getCollection('blog');
+	return rss({
+		title: 'The sanwich news',
+		description: 'All sandwich news',
+		site: context.site as string,
+		items: blog.map((post) => {
+			return {
+				title: post.data.title,
+				pubDate: post.data.date,
+				description: post.data.description,
+				link: `/blog/${post.slug}`,
+				content: sanitizeHtml(parser.render(post.body)),
+			};
+		}),
+	});
+}
